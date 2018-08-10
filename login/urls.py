98d5cc4b55d9ad33d03f7f516b2e39978766f116
Ethan <rainshanghsu@gmail.com>
2018-08-10 17:28:17 +1000

    we decided not to use RESTful

--- login/urls.py
+++ login/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 app_name = 'Login'
 urlpatterns = [
     path('', views.login, name='login'),
-#    path('register/', views.register, name='register'),
-    url(r'^register/$', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('logout/', views.logout, name='logout'),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- miniAirbnb/urls.py
+++ miniAirbnb/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path#, include
-from django.conf.urls import url, include
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('homepage.urls')),
     path('login/', include('login.urls')),
 #    path('search', include('search.urls')),
-#    url(r'user/', include('user.urls')),
     path('user/', include('user.urls')),
     path('admin/', admin.site.urls),
 ]
