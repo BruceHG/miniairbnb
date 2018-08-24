9a4e5c423d0c569d44a76b6522d6429acb8a0ba3
GE HUANG <z5127210@ad.unsw.edu.au>
2018-08-24 23:05:58 +1000

    modify login.views

--- login/urls.py
+++ login/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
-
+#from django.urls import path
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-app_name = 'Login'
+#app_name = 'Login'
+#urlpatterns = [
+#    path('', views.login, name='login'),
+#    path('register/', views.register, name='register'),
+#]
+
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    url(r'', views.login),
 ]
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
--- miniAirbnb/urls.py
+++ miniAirbnb/urls.py
@@ -16,11 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls import url
+
 urlpatterns = [
     path('', include('homepage.urls')),
-    path('login/', include('login.urls')),
+    url(r'^login/', include('login.urls')),
 #    path('search', include('search.urls')),
     # path('user/', include('user.urls')),
     # path('hostadmin/', include('hostadmin.urls')),
     # path('admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+#urlpatterns = [
+#    url(r'^', include(router.urls)),
+#    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+#]
