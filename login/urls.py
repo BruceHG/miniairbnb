7f4e4ddaec14810ac39831407c249152057c0ddf
GE HUANG <z5127210@ad.unsw.edu.au>
2018-08-25 00:27:05 +1000

    revert unnecessary

--- login/urls.py
+++ login/urls.py
@@ -1,16 +1,9 @@
 #from django.urls import path
 from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-#app_name = 'Login'
-#urlpatterns = [
-#    path('', views.login, name='login'),
-#    path('register/', views.register, name='register'),
-#]
-
+app_name = 'Login'
 urlpatterns = [
-    url(r'', views.login),
+    path('', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
-
-#urlpatterns = format_suffix_patterns(urlpatterns)
