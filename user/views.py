4d8555547ba8318c2b21e0242955e16c737c5ff7
GE HUANG <z5127210@ad.unsw.edu.au>
2018-08-28 20:21:17 +1000

    hostrequest decline, user profile

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 
 from user.models import User, Host
-from user.serializers import profileSerializer
+from user.serializers import profileSerializer, guestDetailSerializer, hostDetailSerializer
 from hostadmin.models import HostRequest
 
 from rest_framework.decorators import api_view
@@ -38,6 +38,33 @@ def becomeHost(request):
                     'msg': 'request has been sent',
                     'data': profile.data,
                     }
+    except User.DoesNotExist:
+        result = {
+                'code': status.HTTP_400_BAD_REQUEST,
+                'msg': 'user not found',
+                }
+    except Exception as e:
+        result = {
+            'code': status.HTTP_400_BAD_REQUEST,
+            'msg': str(e),
+        }
+    return Response(result, status=result['code'])
+
+@api_view(['GET'])
+def profile(request):
+    try:
+        username = request.META.get("HTTP_USERNAME")
+        target = User.objects.get(username = username)
+        if target.status == User.HOST:
+            host = Host.objects.get(user = target)
+            profile = hostDetailSerializer(host)
+        else:
+            profile = guestDetailSerializer(target)
+        result = {
+                'code': status.HTTP_200_OK,
+                'msg': 'request has been sent',
+                'data': profile.data,
+                }
     except User.DoesNotExist:
         result = {
                 'code': status.HTTP_400_BAD_REQUEST,
