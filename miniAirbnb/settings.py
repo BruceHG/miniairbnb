e7b8fa595e559e430bfa985ec72885f7bc8088df
Ethan <rainshanghsu@gmail.com>
2018-08-28 10:58:05 +1000

    add UserStatus enum

--- miniAirbnb/settings.py
+++ miniAirbnb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -66,18 +67,22 @@ WSGI_APPLICATION = 'miniAirbnb.wsgi.application'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_HEADERS = default_headers + (
+    'username',
+)
+
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'miniairbnb',
-#         'USER': 'admin',
-#         'PASSWORD': '123321',
-#         'HOST': 'localhost',
-#         'PORT': '',
+        #         'ENGINE': 'django.db.backends.postgresql',
+        #         'NAME': 'miniairbnb',
+        #         'USER': 'admin',
+        #         'PASSWORD': '123321',
+        #         'HOST': 'localhost',
+        #         'PORT': '',
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
