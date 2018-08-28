53c4132d702d8d641bf1a44e065b8acf3fad5b1b
Ethan <rainshanghsu@gmail.com>
2018-08-28 19:32:59 +1000

    recorrect the url path

--- hostadmin/urls.py
+++ hostadmin/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'Hostadmin'
 urlpatterns = [
-    path('', views.hostAdmin, name='hostAdmin'),
+    path('requests', views.hostAdmin, name='hostAdmin'),
     path('approve/', views.approve, name='approve'),
 #    path('adminlogin/', views.adminLogin, name='adminLogin'),
 ]
