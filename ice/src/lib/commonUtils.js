4549e577c0c9344461c78557a617d2dc72892021
Ethan <rainshanghsu@gmail.com>
2018-09-11 14:11:58 +1000

    opps, I did it...

--- ice/src/lib/commonUtils.js
+++ ice/src/lib/commonUtils.js
@@ -1,6 +1,6 @@
 import Cookies from 'universal-cookie';
 
-const DEBUG_IN_LOCAL = true;
+const DEBUG_IN_LOCAL = false;
 const COOKIE_KEY_CURRENT_USER = 'COOKIE_KEY_CURRENT_USER';
 
 export const BACKEND_URL = DEBUG_IN_LOCAL ? 'http://login.cse.unsw.edu.au:8007' : '';
